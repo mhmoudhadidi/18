6b2c21a8166a9948997eaa4520e12d2829352976
mhmoudhadidi <134384059+mhmoudhadidi@users.noreply.github.com>
2023-05-23 14:41:37 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+if input.temperature() > 0:
+    basic.show_string("the temperature is above zero")
+    if input.temperature() > 0:
+        basic.show_string("the temperature is below zero")
+    else:
+        basic.show_string("the temperature is zero")
---
+++ main.ts
@@ -0,0 +1,10 @@
+if (input.temperature() > 0) {
+    basic.showString("the temperature is above zero")
+    if (input.temperature() > 0) {
+        basic.showString("the temperature is below zero")
+    } else {
+        basic.showString("the temperature is zero")
+    }
+    
+}
+
